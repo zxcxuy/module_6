aedc51cf22591c5655f4bc56e3ee58ecfa071994
dima <140681941+zxcxuy@users.noreply.github.com>
2025-08-10 18:58:59 +0300

    Delete 4.js

--- 4.js
+++
@@ -1,9 +0,0 @@
-const poleArr = [["x", "o", "x"],["o", "x", "o"],["x", "o", "x"]];
-let str = "";
-for(let i = 0; i < 3; i++) {
-  for(let j = 0; j < 3; j++){
-    str += `${poleArr[i][j]} `;
-  }
-  str += "\n";
-}
-console.log(str)
